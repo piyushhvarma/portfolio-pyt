b551dca1ca932a71d5930236ef4e147164422eb6
Piyush Varma <pytworks@gmail.com>
2026-03-03 13:58:08 +0530

    feat: add initial resume data and generate package-lock.json.

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -19,7 +19,7 @@ export const DATA = {
   location: "Jaipur, IN",
   locationLink: "https://www.google.com/maps/place/jaipur",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer and product builder passionate about AI and problem-solving. I love creating things that people actually use, sharing insights online, and learning openly along the journey.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
@@ -46,14 +46,14 @@ export const DATA = {
     social: {
       GitHub: {
         name: "GitHub",
-        url: "https://github.com/piyushhvarma",
+        url: "https://github.com/piyushhvarma/",
         icon: Icons.github,
         navbar: true,
       },
 
       LinkedIn: {
         name: "LinkedIn",
-        url: "https://dub.sh/dillion-linkedin",
+        url: "https://linkedin.com/in/piyushhvarma/",
         icon: Icons.linkedin,
 
         navbar: true,
