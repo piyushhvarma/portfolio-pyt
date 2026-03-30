c875390c78ea32f973ef5e064778b8630840c89c
Piyush Varma <pytworks@gmail.com>
2026-03-30 15:39:33 +0530

    implemented hypertext onto heading and made it interactive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import BlurFade from "@/components/magicui/blur-fade";
+import { HyperText } from "@/components/ui/hyper-text";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { DATA } from "@/data/resume";
@@ -20,12 +21,13 @@ export default function Page() {
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 gap-y-6 flex flex-col md:flex-row justify-between">
             <div className="gap-2 flex flex-col order-2 md:order-1">
-              <BlurFadeText
+              <BlurFade
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-semibold tracking-tighter sm:text-4xl lg:text-5xl"
                 yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(" ")[0]}`}
-              />
+              >
+                Hi, I&apos;m <HyperText className="inline-block">{DATA.name.split(" ")[0]}</HyperText>
+              </BlurFade>
               <BlurFadeText
                 className="text-muted-foreground max-w-[600px] md:text-lg lg:text-xl"
                 delay={BLUR_FADE_DELAY}
