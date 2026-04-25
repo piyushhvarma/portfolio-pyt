25084518997c79a9b84107d4c581f2091870b8c7
Piyush Varma <pytworks@gmail.com>
2026-04-26 02:33:55 +0530

    fix: change react-github-calendar from default export to named export to fix build error

--- src/components/section/github-calendar-section.tsx
+++ src/components/section/github-calendar-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import GitHubCalendar from "react-github-calendar";
+import { GitHubCalendar } from "react-github-calendar";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
