71b5c44a61744df767dd4ee7b52a5a2c799438e0
Piyush Varma <pytworks@gmail.com>
2026-04-26 21:48:24 +0530

    feat: use animated icons for theme toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { cn } from "@/lib/utils";
+import BrightnessDownIcon from "@/components/ui/svgs/brightnessDownIcon";
+import MoonIcon from "@/components/ui/svgs/moonIcon";
 
 export function ModeToggle({ className }: { className?: string }) {
   const { theme, setTheme } = useTheme();
@@ -16,8 +17,8 @@ export function ModeToggle({ className }: { className?: string }) {
       className={cn("size-8 rounded-full bg-background/50 backdrop-blur-md border border-border/50", className)}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      <Sun className="size-4 dark:hidden" />
-      <Moon className="hidden size-4 dark:block" />
+      <BrightnessDownIcon size={16} className="dark:hidden" />
+      <MoonIcon size={16} className="hidden dark:block" />
     </Button>
   );
 }
